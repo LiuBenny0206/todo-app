1ae0d13954b52c58ffa48d3c10797d1259bdf1cd
LiuBenny0206 <karta30121@gmail.com>
2024-04-09 21:16:30 +0800

    set up

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import '../output.css';
+
+function App (){
+    return (
+        <div>
+            <p>Hello!</p>
+        </div>
+    );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"], // 這樣配置可以捕獲 src 資料夾下所有的 .js 和 .jsx 文件
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
